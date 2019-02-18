903fc6b4e00f8d0e486862bb878420a58bcc06e4
KennedyDias <kennedysouzadias@gmail.com>
2019-02-18 20:15:25 -0300

    Created module of calculator

---
+++ cravecalculator/src/main/java/com/cravefood/cravecalculator/CalculatorFragment.kt
@@ -0,0 +1,32 @@
+package com.cravefood.cravecalculator
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class CalculatorFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = CalculatorFragment()
+    }
+
+    private lateinit var viewModel: CalculatorViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.calculator_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(CalculatorViewModel::class.java)
+
+    }
+
+}
---
+++ cravecalculator/src/main/java/com/cravefood/cravecalculator/CalculatorViewModel.kt
@@ -0,0 +1,7 @@
+package com.cravefood.cravecalculator
+
+import androidx.lifecycle.ViewModel;
+
+class CalculatorViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
