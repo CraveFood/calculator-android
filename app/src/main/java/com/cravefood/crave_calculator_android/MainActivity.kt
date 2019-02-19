0ff25bcb4ac4eddb65365dd7bbe97da80f2a81e5
KennedyDias <kennedysouzadias@gmail.com>
2019-02-19 13:56:56 -0300

    Make an initial project

--- app/src/main/java/com/cravefood/crave_calculator_android/MainActivity.kt
+++ app/src/main/java/com/cravefood/crave_calculator_android/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.cravefood.crave_calculator_android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.cravefood.cravecalculator.CalculatorFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().add(
+            R.id.frameLayout,
+            CalculatorFragment.newInstance()
+        ).commit()
     }
 }
